6292dc90305a1cdede12f84de3ec6f99b5d120fa
Himanshu <himanshukakran8@gmail.com>
2025-07-08 00:16:00 +0530

    Add API for frontend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:3000/api' });
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api',
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
